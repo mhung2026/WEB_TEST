86a017c688bd39bf217f5294c1e6065440dc4b6b
Minh Hưng <124641608+mhung2026@users.noreply.github.com>
2023-07-24 19:26:18 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com/nguyenphan299";
+            window.location = "http://fb.com/hung2026";
           },
         });
       }
